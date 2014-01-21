f8ae5475dd21cdd31feb282b5c6ce234c91dfdc4
Ian Flynn <himitsu.monban@gmail.com>
2014-01-21 01:33:54 -0500

    added a new facility page with some basic file-saving capabilities for maps

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,5 +1,24 @@
 class FacilitiesController < ApplicationController
+  def new
+    @entity = Facility.new
+  end
+  
+  def create
+    @entity = Facility.new(params.require(:facility).permit(allowed_params))
+    if @entity.save
+      redirect_to @entity
+    else
+      render action: 'new'
+    end
+  end
+  
   def show
     @entity = Facility.find(params[:id])
+  end
+  
+  private
+  
+  def allowed_params
+    [:name, :picture]
   end
 end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,3 +1,20 @@
 class Facility < ActiveRecord::Base
   has_many :deployments
+  validate :has_image?
+  attr_accessor :picture
+  after_create :save_picture
+  
+  def save_picture
+    unless picture.nil?
+      self.image_file = Rails.root.join('public', 'maps', "#{id}.jpg").to_s
+      File.open(image_file, 'wb') do |file|
+        file.write picture.read
+      end
+    end
+    true
+  end
+  
+  def has_image?
+    image_file || picture
+  end
 end
